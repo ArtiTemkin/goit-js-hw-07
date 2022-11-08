6876ea372d64568a6c6d32e3f34389355110316f
ArtiTemkin <a.zymarev@icloud.com>
2022-11-09 00:51:00 +0200

    Первая сдача ДЗ / проблема с запятой

---
+++ src/js/01-gallery.js
@@ -0,0 +1,64 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+
+console.log(galleryItems);
+
+const galeryContainer = document.querySelector('.gallery');
+const picturesMarkup = createPicturesLibrary(galleryItems);
+
+galeryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+
+galeryContainer.addEventListener('click', openModalByClick)
+
+function createPicturesLibrary(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+            <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+            </div> 
+        `
+            
+    }).join('')
+    // console.log(pictures)
+}
+
+function openModalByClick(event) {
+    // const isPictureEl = event.target.classlist.contains('gallery__image')
+    console.log(event.target.nodeName)
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+     event.preventDefault();
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <img src="${event.target.dataset.source}" width="800" heigth="600";
+        </div>
+    `)
+
+    instance.show()
+
+    window.addEventListener("keydown", closeModal)
+
+    function closeModal(event) {
+        console.log(event.code)
+
+        if (event.code === "Escape") {
+            instance.close()
+        }
+    }
+}
+
+
+
+
---
+++ src/js/02-lightbox.js
@@ -0,0 +1,44 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galeryContainer = document.querySelector('.gallery');
+const createGalary = creatingPictures(galleryItems) 
+
+galeryContainer.insertAdjacentHTML('beforeend', createGalary)
+// gallery.addEventListener('click', openModal)
+
+function creatingPictures(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+    })
+}
+
+// function openModal(event) {
+//     console.log(event.target.nodeName)
+
+//     if (event.target.nodeName !== "IMG") {
+//         return
+//     }
+
+//     event.preventDefault()
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250
+});
+
+//     gallery.on('show.simplelightbox', function () {
+	
+// });
+
+    //  const instance = basiclightbox.create(`
+    //     <div class="modal">
+    //         <img src="${event.target.dataset.source}" width="800" heigth="600";
+    //     </div>
+    // `)
+    // instance.show()
+// }
